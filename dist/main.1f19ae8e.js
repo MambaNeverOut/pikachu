1559acb19ce446e5e799a5972ae760cefb47dce7
Irelia <yaokai729@outlook.com>
2020-04-05 11:41:28 +0800

    重新打包

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -229,7 +229,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "13962" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "14261" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -406,4 +406,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["C:/Users/86138/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js","main.js"], null)
-//# sourceMappingURL=/main.1f19ae8e.js.map
+//# sourceMappingURL=main.1f19ae8e.js.map
